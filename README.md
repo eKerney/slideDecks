c1cf5cd52f61daa3d6ccc59e40d25035d01e39b9
eKerney <Eric.Kerney@airspacelink.com>
2022-09-20 10:24:19 -0400

    A new codebase says Hello Earth!

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #  slideDecks.git
+# 
--- newRepo.sh
+++ newRepo.sh
@@ -1,15 +1,8 @@
 #! /usr/bi/env bash
-echo "# " $2 >> README.md
-cat README.md
-git init
-echo "git init"
 git add .
 echo "added files"
-git commit -m "A new codebase says Hello Earth!"
-echo "git commit -m 'A new codebase says Hello Earth!'"
-git branch -M main
-git remote add origin https://github.com/$1/$2
-echo "git remote add origin https://github.com/$1/$2"
+git commit -m $1
+echo git commit -m $1
 git push -u origin main
 echo "git push -u origin main"
 
